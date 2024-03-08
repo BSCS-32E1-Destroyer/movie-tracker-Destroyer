7aab5caba6bb36688650a82d3b1f889ebcae2743
Warren Albino <warrenalbino277@gmail.com>
2024-03-08 22:25:58 +0800

    First check

---
+++ movie-tracker/movie-tracker/Controllers/HelloWorldController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers;
+
+public class HelloWorldController : Controller
+{
+    // 
+    // GET: /HelloWorld/
+    public string Index()
+    {
+        return "This is my default action...";
+    }
+    // 
+    // GET: /HelloWorld/Welcome/ 
+   public string Welcome(string name, int numTimes = 1)
+{
+    return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+}
+}
