a256d15d3051028a0d1ca9a8587f424f8ef588ee
Warren Albino <warrenalbino277@gmail.com>
2024-03-21 15:11:10 +0800

    Added search by Genre

---
+++ movie-tracker/movie-tracker/Models/MovieGenreViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using movie_tracker.Models;
+using System.Collections.Generic;
+
+namespace movie_tracker.Models;
+
+public class MovieGenreViewModel
+{
+    public List<Movie>? Movies { get; set; }
+    public SelectList? Genres { get; set; }
+    public string? MovieGenre { get; set; }
+    public string? SearchString { get; set; }
+}
